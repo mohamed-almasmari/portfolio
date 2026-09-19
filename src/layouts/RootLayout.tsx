f8682f1a7adaa508b91f78b9f3c44c8dee7bb275
Mohamed Almasmari <191190813+mohamed-almasmari@users.noreply.github.com>
2026-09-19 16:42:53 -0400

    Reset scroll position on route changes

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import ScrollToHash from "../components/ScrollToHash/ScrollToHash";
+import ScrollToHash from "../components/ScrollToHash/ScrollToHash.tsx";
+import ScrollToTop from "../components/ScrollToHash/ScrollToTop.tsx";
 
 function RootLayout() {
   return (
     <div className="min-h-screen bg-slate-950">
+      <ScrollToTop />
       <ScrollToHash />
 
       <Navbar />
